f4d98957fac5d061b556ea27a6441adc55d632ed
Chinosu <timtang08@gmail.com>
2023-07-31 19:05:11 +1000

    added program files

---
+++ warframe.Drift/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Configuration;
+global using System.Data;
+global using System.Linq;
+global using System.Threading.Tasks;
+global using System.Windows;
+global using System.Windows.Controls;
+global using System.Windows.Data;
+global using System.Windows.Documents;
+global using System.Windows.Input;
+global using System.Windows.Media;
+global using System.Windows.Media.Imaging;
+global using System.Windows.Navigation;
+global using System.Windows.Shapes;
+global using System.Runtime.CompilerServices;
+global using System.ComponentModel;
+global using System.Diagnostics;
+global using System.Timers;
+
+namespace warframe.Suspender;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
---
+++ warframe.Drift/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System.Threading;
+
+namespace warframe.Suspender;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window, INotifyPropertyChanged
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    internal const string TargetProcessName = "Warframe.x64";
+
+    internal readonly TimeSpan Duration = new(0, 40, 0);
+    internal readonly TimeSpan DownTime = new(0, 0, 30);
+
+    internal System.Timers.Timer timer = default!;
+
+    public bool CanStart
+    {
+        get => _canStart;
+        set
+        {
+            _canStart = value;
+            OnPropertyChanged(nameof(CanStart));
+        }
+    }
+    private bool _canStart = true;
+
+    public bool CanStop
+    {
+        get => _canStop;
+        set
+        {
+            _canStop = value;
+            OnPropertyChanged(nameof(CanStop));
+        }
+    }
+    private bool _canStop = false;
+
+    public string Status
+    {
+        get => _status;
+        set
+        {
+            _status = value;
+            OnPropertyChanged(nameof(Status));
+        }
+    }
+    private string _status = "Inactive!";
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string name = null!) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+    private void OnStart(object sender, RoutedEventArgs e)
+    {
+        Suspend();
+        CanStart = !CanStart;
+        CanStop = !CanStop;
+        Status = "Active!";
+
+        timer = new(Duration.TotalMilliseconds);
+        timer.Elapsed += (s, e) =>
+        {
+            UnSuspend();
+            Status = "Refreshing";
+            Thread.Sleep(Convert.ToInt32(DownTime.TotalMilliseconds));
+            Suspend();
+            Status = "Active!";
+        };
+        timer.AutoReset = true;
+        timer.Start();
+    }
+    private void OnStop(object sender, RoutedEventArgs e)
+    {
+        Status = "Inactive!";
+        UnSuspend();
+        CanStart = !CanStart;
+        CanStop = !CanStop;
+        timer.Stop();
+    }
+
+    private void Suspend() => Process.GetProcessesByName(TargetProcessName).ToList().ForEach(p => ProcessManager.Suspend(p.Id));
+    private void UnSuspend() => Process.GetProcessesByName(TargetProcessName).ToList().ForEach(p => ProcessManager.Resume(p.Id));
+}
